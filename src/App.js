6ed8fa3e09c8026d1bfee3a8041ffd00c25627cd
Vesta-Nassone <vestanassone@gmail.com>
2024-09-21 16:01:32 +0200

    Render the footer section.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
+import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
-import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Header />
       <MainContent/>
+      <Footer />
     </div>
   );
 }
